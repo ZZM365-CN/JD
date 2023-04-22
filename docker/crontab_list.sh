a2bcfba38ebfabd63c9e3fe4b7e3bf03d448de4c
ZZMISKING <57690855+ZZMISKING@users.noreply.github.com>
2023-04-22 19:47:00 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,24 +1,24 @@
-30 21 * * * jd_bean_info.js
-0 7 * * * jd_bean_sign.js
-15 6-18/6 * * * jd_fruit.js
-0 7,19 * * * jd_speed_sign.js
-45 0 * * * jd_speed_sign.ts
-15 7-21/2 * * * jd_plantBean.js
-41 21 * * * jd_prices.js
-23 1,18 * * * jd_bean_home.js
-20 13 * * 6 jd_get_share_code.js
-13 0,6,22 * * * jd_health.js.
-5 4,14 * * * jd_health_help.js
-3 1-23 * * * jd_joy_park_run.js
-30 0 * * * jd_joy_park_run.ts
-21 9 * * * jd_gua_MMdou_Mod.js
-9 8 * * * jd_plus2bean.js
-21 3 * * * jd_signbeanact.js
-30 7 * * * jd_jrsign.js
-30 18 * * * jd_joypark_synthesis.js
-50 7 * * * jd_joypark_task.js
-35 9 * * * jd_joy_withdraw.js
-35 8 * * * jd_dygetbeans.js
-5 5,10 * * * jd_club_lottery.js
-35 9 * * * jd_cash.js
-6 0,5 * * * jd_cash_wx.js
+30 21 * * * task jd_bean_info
+0 7 * * * task jd_bean_sign
+15 6-18/6 * * * task jd_fruit
+0 7,19 * * * task jd_speed_sign
+45 0 * * * task jd_speed_sign
+15 7-21/2 * * * task jd_plantBean
+41 21 * * * task jd_prices
+23 1,18 * * * task jd_bean_home
+20 13 * * 6 task jd_get_share_code
+13 0,6,22 * * * task jd_health
+5 4,14 * * * task jd_health_help
+3 1-23 * * * task jd_joy_park_run
+30 0 * * * task jd_joy_park_run
+21 9 * * * task jd_gua_MMdou_Mod
+9 8 * * * task jd_plus2bean
+21 3 * * * task jd_signbeanact
+30 7 * * * task jd_jrsign
+30 18 * * * task jd_joypark_synthesis
+50 7 * * * task jd_joypark_task
+35 9 * * * task jd_joy_withdraw
+35 8 * * * task jd_dygetbeans
+5 5,10 * * * task jd_club_lottery
+35 9 * * * task jd_cash
+6 0,5 * * * task jd_cash_wx
