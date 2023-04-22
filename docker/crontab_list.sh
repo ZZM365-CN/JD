942c3a05b43422909e612cb4559ca748bae98041
ZZMISKING <57690855+ZZMISKING@users.noreply.github.com>
2023-04-22 13:01:27 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,4 +1,4 @@
-30 21 * * * jd_bean_change.js
+30 21 * * * jd_bean_info.js
 0 7 * * * jd_bean_sign.js
 15 6-18/6 * * * jd_fruit.js
 0 7,19 * * * jd_speed_sign.js
@@ -10,5 +10,15 @@
 13 0,6,22 * * * jd_health.js.
 5 4,14 * * * jd_health_help.js
 3 1-23 * * * jd_joy_park_run.js
-30 0 * * * * jd_joy_park_run.ts
+30 0 * * * jd_joy_park_run.ts
 21 9 * * * jd_gua_MMdou_Mod.js
+9 8 * * * jd_plus2bean.js
+21 3 * * * jd_signbeanact.js
+30 7 * * * jd_jrsign.js
+30 18 * * * jd_joypark_synthesis.js
+50 7 * * * jd_joypark_task.js
+35 9 * * * jd_joy_withdraw.js
+35 8 * * * jd_dygetbeans.js
+5 5,10 * * * jd_club_lottery.js
+35 9 * * * jd_cash.js
+6 0,5 * * * jd_cash_wx.js
