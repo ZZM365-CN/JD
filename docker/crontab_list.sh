228b92723a9548ef014e6b716622e831feff8299
Ming <57690855+ZZM365-CN@users.noreply.github.com>
2023-05-17 08:31:12 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,6 +6,7 @@
 30 21 * * * node /scripts/jd_bean_info.js >> /scripts/logs/jd_info.log 2>&1
 0 7 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 20 4,16 * * * node /scripts/jd_fruit_help.js >> /scripts/logs/jd_fruit_help.log 2>&1
+5 6-18/6 * * * node /scripts/jd_fruit_task.js >> /scripts/logs/jd_fruit_task.log 2>&1
 10 7-22/1 * * * node /scripts/jd_plantBean.js >> /scripts/logs/jd_plantBean.log 2>&1
 41 21 * * * node /scripts/jd_prices.js >> /scripts/logs/jd_prices.log 2>&1
 23 1,18 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
